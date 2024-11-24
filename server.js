7ff57a0ad281334963f66dd1649fd949f85a7e79
Himanshu Kumar <143962962+Himanshu-vashist@users.noreply.github.com>
2024-11-24 14:49:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ dotenv.config();
 //databse config
 connectDB();
 
+
+const __filename =fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 //rest object
 const app = express();
 
